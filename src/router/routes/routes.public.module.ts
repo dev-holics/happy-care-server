416fa8bc5c796adab25cdbfe4598e2d662af0de0
locnguyen0904 <thailoc321@gmail.com>
2022-10-24 17:22:33 +0700

    fix: conflict

--- src/router/routes/routes.public.module.ts
+++ src/router/routes/routes.public.module.ts
@@ -1,13 +1,19 @@
+import { ProductModule } from 'src/modules/product/product.module';
 import { Module } from '@nestjs/common';
 import { UserPublicController } from 'src/modules/user/controllers/user.public.controller';
 import { UserModule } from 'src/modules/user/user.module';
 import { CategoryModule } from 'src/modules/category/category.module';
 import { CategoryPublicController } from 'src/modules/category/controllers/category.public.controller';
+import { ProductPublicController } from 'src/modules/product/controllers';
 
 @Module({
-	controllers: [UserPublicController, CategoryPublicController],
+	controllers: [
+		UserPublicController,
+		CategoryPublicController,
+		ProductPublicController,
+	],
 	providers: [],
 	exports: [],
-	imports: [UserModule, CategoryModule],
+	imports: [UserModule, CategoryModule, ProductModule],
 })
 export class RoutesPublicModule {}
