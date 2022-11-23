0cc67684809ae28839f200ca985fd3d3c0ad19d5
Linh Huynh Thi Ai <huynhthiailinh2105@gmail.com>
2022-11-23 21:36:13 +0700

    fix: refactor category dtos index

--- src/modules/category/dtos/index.ts
+++ src/modules/category/dtos/index.ts
@@ -1,5 +1,5 @@
 export * from './category.create.dto';
 export * from './category.input.param.dto';
 export * from './category.input.query.dto';
-export * from './category.update.dto';
 export * from './category.list.query.dto';
+export * from './category.update.dto';
